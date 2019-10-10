50ae2c1ccc3661884ca11db533cf45e5d574d143
Ariana <ariana.carnielli@gmail.com>
2019-10-10 19:08:41 +0200

    debut projet

---
+++ projet.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 10 16:33:23 2019
+
+@author: Ariana CARNIELLI
+"""
+
+"""
+Méthodes initiales pour le projet de COMPLEX
+"""
+
+
+import networkx as nx
+
+class Graphe:
+    
+    def __init__(self):
+        """
+        """
+        self.graphe = nx.Graph()
+
+    
+    def readfile(self, nomfichier):
+        """
+        """
+        with open(nomfichier, 'r') as file:
+            assert file.readline() == "Nombre de sommets\n"
+            nbSommets = int(file.readline())
+            assert file.readline() == "Sommets\n"
+            
+            for i in range(nbSommets):
+                nomSommet = int(file.readline())
+                self.graphe.add_node(nomSommet)
+ 
+            assert file.readline() == "Nombre d aretes\n"
+            nbAretes = int(file.readline())
+            assert file.readline() == "Aretes\n"
+            
+            for i in range(nbAretes):
+                line = file.readline()
+                debut, fin = line.split(" ")
+                debut = int(debut)
+                fin = int(fin)
+                self.graphe.add_edge(debut, fin)
+    
+    
+    def supprimirSommet(self, sommet):
+        """
+        """
+        g2 = Graphe()
+        g2.graphe = nx.Graph(self.graphe)
+        g2.graphe.remove_node(sommet)
+        
+        return g2
+        
+    def supprimirSommets(self, sommets):
+        """
+        """
+        g2 = Graphe()
+        g2.graphe = nx.Graph(self.graphe)
+        g2.graphe.remove_nodes_from(sommets)
+        return g2   
+           
+    def degresSommet(self):
+        """
+        """
+        return dict(self.graphe.degree())
+    
+    def degreMax(self):
+        """
+        """
+        degres = self.degresSommet()
+        max(degres, key=lambda key: stats[key])
+
+
+            
+            
+            
+            
+            
+        
+            
